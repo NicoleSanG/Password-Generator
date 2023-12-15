601e5006b28a0776a76853af9b4e279204a96ca8
NicoleSG <nicolesantamariagranizo@gmail.com>
2023-12-15 21:23:11 +0000

    added code to check if at least one character set is selected

--- script.js
+++ script.js
@@ -97,50 +97,58 @@ var upperCasedCharacters = [
 // At least one character type should be selected.
 
 // Function to prompt user for password options
+// Prompt for password length
+// At least 8 characters, no more than 128 characters
+// Conditional to check that the number that was entered is in range
+// Prompts store data as strings, so need to parse into a number
+// If the user's input is out of range, either return out of the function or call the function again
+// Confirm which character sets to use
+// If the user answers false for all, either return out of the function or call the function again
+// Once they select a character set:
+// Generate a random character for each selected character set
+// Either push selected character sets to a mega-array of all selected characters
+// OR you can keep the arrays separate and generate a random number to select the array and another to select the index
+// Once character sets are selected, move on to generating random characters
 function getPasswordOptions() {
-  // Prompt for password length
-  // At least 8 characters, no more than 128 characters
-  // Conditional to check that the number that was entered is in range
-  // Prompts store data as strings, so need to parse into a number
-  // If the user's input is out of range, either return out of the function or call the function again
-  // Confirm which character sets to use
-  // If the user answers false for all, either return out of the function or call the function again
-  // Once they select a character set:
-  // Generate a random character for each selected character set
-  // Either push selected character sets to a mega-array of all selected characters
-  // OR you can keep the arrays separate and generate a random number to select the array and another to select the index
-  // Once character sets are selected, move on to generating random characters
+
   let passwordLength = parseInt(prompt('Please enter password length(between 8 and 128 characters):'));
   if (isNaN(passwordLength) || passwordLength < 8) {
     alert('Please enter a valid number.');
     return;
   }
   if (isNaN(passwordLength > 128)) {
-    alert('Please, enter a number between 8 and 128.')
+    alert('Please, enter a number between 8 and 128.');
   }
-  return;}
+  return;
+}
 
+let includeLowercase = confirm("Would you like to include lowercase letters?");
+let includeUppercase = confirm("Would you like to include uppercase letter?");
+let includeNumbers = confirm("Would you like to include any numbers?");
+let specialCharacters = confirm("Would you like to include any special characters?");
 
-  let includeLowercase = confirm("Would you like to include lowercase letters? (Click 'OK' for Yes or 'Cancel' for No)");
-  let includeUppercase = confirm("Would you like to include uppercase letter? (Click 'OK' for Yes or 'Cancel' for No)");
-  let includeNumbers = confirm("Would you like to include any numbers? (Click 'OK' for Yes or 'Cancel' for No)");
-  let specialCharacters = confirm("Would you like to include any special characters? (Click 'OK' for Yes or 'Cancel' for No)");
+if (includeLowercase === false &&
+  includeUppercase === false &&
+  includeNumbers === false &&
+  specialCharacters === false
+) {
+  alert("At least 1 character type should be selected, please try again!")
+  getPasswordOptions();
 
-
+}
 
 
 // Function for getting a random element from an array
-function getRandom(arr) {
-  // Need a variable to hold the password as it's being generated
+// Need a variable to hold the password as it's being generated
   // Need a variable to hold the index that's being generated
-
   // For loop that loops the number of times that matches the length the user chose
   // Generate a random number
   // That number is the index for a character in the mega-array
   // So then, mega-array[generated-index] is the actual character
   // Add that character to the password
-
   // Once we finish the for loop, return the generated password
+function getRandom(arr) {
+  
 }
 
 // Function to generate password with user input
@@ -161,3 +169,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
+
+
